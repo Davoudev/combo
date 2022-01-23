46410acd4a973e8395d4dc405261ccc5e721e216
David <davidchaharmehali@gmail.com>
2022-01-23 16:29:35 +0330

    frist commit

---
+++ combo.js
@@ -0,0 +1,60 @@
+let h = document.getElementById("h");
+const message = document.querySelector(".message");
+const gamearea = document.querySelector(".game");
+const button = document.querySelector("button");
+let gameplay = false;
+let score = 0;
+
+button.addEventListener("click", fun);
+function fun() {
+  if (!gameplay) {
+    gameplay = true;
+    maker();
+
+    button.innerHTML = "check combo";
+
+    score = 0;
+  } else {
+    score++;
+
+    message.innerHTML = "guess" + score;
+
+    const numbers = document.querySelectorAll(".numb");
+
+    let wincondition = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i].value == numbers[i].correct) {
+        numbers[i].style.backgroundColor = "green";
+
+        wincondition++;
+      } else {
+        let color = numbers[i].value < numbers[i].correct ? "blue" : "red";
+
+        numbers[i].style.backgroundColor = color;
+
+        numbers[i].style.color = "black";
+      }
+      if (wincondition == numbers[i].length) {
+    
+      
+      }
+    }
+  }
+}
+
+function maker() {
+  for (let x = 0; x < 5; x++) {
+    let el = document.createElement("input");
+    el.setAttribute("type", "number");
+    el.max = 9;
+    el.min = 0;
+    el.size = 1;
+    el.style.width = "50px";
+    el.classList.add("numb");
+    el.correct = Math.floor(Math.random() * 10);
+    el.value = 0;
+    el.order = x;
+    gamearea.appendChild(el);
+  }
+}
